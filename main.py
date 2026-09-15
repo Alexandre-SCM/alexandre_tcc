331208663f4f14ed3152dc69ab6cae649bcaeea4
alexandre menezes <alex.menezes1722@gmail.com>
2026-09-15 14:17:31 -0300

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import pandas as pd
+
 print("Hello, World!")
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print ("Você é maior de idade.")
+else:
+    print ("Você é menor de idade.")
+
+
+#.\venv\Scripts\Activate.ps1
